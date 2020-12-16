9239e7b636bcc742f5d628fc866fd685d22742d9
nick jenkins <nickjenkinsweb@gmail.com>
2020-12-15 23:52:01 -0500

    tweak tmux setup script pane numbers

--- tmux-setup.sh
+++ tmux-setup.sh
@@ -2,9 +2,9 @@
 export ${WATCH_DIR:=$PWD}
 tmux new -s 'watch' \; \
     setenv WATCH_DIR $WATCH_DIR \; \
-    send-keys -t 0 "export WATCH_DIR="$WATCH_DIR C-m \; \
+    send-keys -t 1 "export WATCH_DIR="$WATCH_DIR C-m \; \
     split-window -h \; \
     split-window -v \; \
-    send-keys -t 0 'cd $WATCH_DIR' C-m \; \
     send-keys -t 1 'cd $WATCH_DIR' C-m \; \
-    send-keys -t 2 'node ~/Git/njw/micro-server/server.js -s -h '$WATCH_DIR \;
+    send-keys -t 2 'cd $WATCH_DIR' C-m \; \
+    send-keys -t 3 'node ~/Git/njw/micro-server/server.js -s -h '$WATCH_DIR \;
