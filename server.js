048fdd4bb99f11242adef0f6d61b00686b954650
nick jenkins <nickjenkinsweb@gmail.com>
2022-02-01 18:42:09 -0800

    update dependencies

--- server.js
+++ server.js
@@ -2,18 +2,25 @@ const path = require('path');
 const commander = require('commander');
 const buildServer = require('./src/buildServer');
 
-commander.version('0.1.0')
-  .usage('[options] <rootpath>')
-  .option('-p, --port [port]', 'Set the server port', 3020)
-  .option('-f --forwardport [port]', 'The forwarding port for the mask middleware', 3040)
-  .option('-s --websocket', 'Use the websocket broadcaster')
-  .option('-h --html', 'serve html files without extension on url')
-  .parse(process.argv);
+const program = new commander.Command();
 
-const rootPath = commander.args[0] ? path.resolve(__dirname, commander.args[0]) : null;
-const port = commander.port;
-const forwardPort = commander.forwardport;
-const useWebsocket = commander.websocket;
-const serveHtml = commander.html;
+program.name('micro-server')
+  .version('0.1.0')
+  .usage('[options] <rootpath>')
+  .argument('<rootpath>')
+  .option('-p, --port [port]', 'Set the server port', 3020)
+  .option('-f, --forwardport [port]', 'The forwarding port for the mask middleware', 3040)
+  .option('-s, --websocket', 'Use the websocket broadcaster')
+  .option('-h, --html', 'Serve html files without extension on url')
+  .parse();
+
+const options = program.opts();
+
+const port = options.port;
+const forwardPort = options.forwardport;
+const useWebsocket = options.websocket;
+const serveHtml = options.html;
+
+const rootPath = program.args[0] ? path.resolve(__dirname, program.args[0]) : null;
 
 buildServer({rootPath, port, forwardPort, useWebsocket, serveHtml});
